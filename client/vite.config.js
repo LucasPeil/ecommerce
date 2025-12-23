d927155a76e3ca7e1c5b605dd424552afca85378
Lucas Chagas <lucaspeil0209@gmail.com>
2025-12-23 00:04:11 -0300

    fixing vercel.json file

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/forniture',
+  // base: '/forniture', // This was causing the issue on Vercel root deploy
+  base: '/',
+ /*  build: {
+    outDir: '../public',
+    emptyOutDir: true,
+  }, */
   server: {
     port: 3100,
     proxy: {
